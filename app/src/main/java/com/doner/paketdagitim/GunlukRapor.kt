e71069a29254a1e14d4627dad0f6240d695b42e2
wolcanic-bit <wolcanic@gmail.com>
2026-05-13 10:12:51 +0300

    Update GunlukRapor.kt

--- app/src/main/java/com/doner/paketdagitim/GunlukRapor.kt
+++ app/src/main/java/com/doner/paketdagitim/GunlukRapor.kt
@@ -7,12 +7,18 @@ import androidx.room.PrimaryKey
 data class GunlukRapor(
     @PrimaryKey
     val tarih: String,
+    val nokta1Isim: String = "",
     val nokta1Normal: Int = 0,
     val nokta1Ekonomik: Int = 0,
+    val nokta1Tutar: Int = 0,
+    val nokta2Isim: String = "",
     val nokta2Normal: Int = 0,
     val nokta2Ekonomik: Int = 0,
+    val nokta2Tutar: Int = 0,
+    val nokta3Isim: String = "",
     val nokta3Normal: Int = 0,
     val nokta3Ekonomik: Int = 0,
+    val nokta3Tutar: Int = 0,
     val toplamPaket: Int = 0,
     val toplamKazanc: Int = 0,
     val normalFiyat: Int = 80,
