f350cf364c615f737966de3430ca2d9d5e09e941
wolcanic-bit <wolcanic@gmail.com>
2026-05-13 07:48:28 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.0-1.0.33" apply false
 }
